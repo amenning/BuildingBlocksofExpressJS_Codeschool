04aae585f1337eba0517d56ddea0e0a63bab71ab
Andrew Menning <menning23@gmail.com>
2016-01-18 18:17:57 -0500

    Lesson 2 Express Static Middleware

---
+++ Level2/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+//Example 1 not using middleware
+/*
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/public/index.html');
+});
+*/
+
+//Example 2 - same result as example 1 but using static middleware
+app.use(express.static('public'));
+
+//Example 3
+app.get('/blocks', function(request, response){
+	var blocks = ['Fixed','Movable','Rotating'];
+	response.json(blocks);
+});
+
+
+app.listen(3000, function(){
+	console. log('Listening on port 3000');	
+});
